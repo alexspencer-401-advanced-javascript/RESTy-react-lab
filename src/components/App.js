15479a66d0bd8ea50189ea902752aa4d149b9707
Alex Spencer <alexander.j.spencer@gmail.com>
2019-11-01 11:54:11 -0700

    basic components

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from './Footer';
+import History from '../components/containers/History';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <History />
+      <Footer />
+    </>
+  );
+}
---
+++ src/components/containers/History.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class History extends Component {
+  // state = {
+  
+  // }
+
+  render() {
+    return (
+      <h1>History</h1>
+    );
+  }
+}
