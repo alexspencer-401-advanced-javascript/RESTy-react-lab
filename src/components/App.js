f345e9fe5dd8a2e40a983a5f22e4c44e0b98fdb8
sanvean74 <dlambertpdx@gmail.com>
2019-11-01 13:44:55 -0700

    form created

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from './Footer';
 import History from '../components/containers/History';
+import Form from '../components/containers/Form';
 
 export default function App() {
   return (
     <>
       <Header />
       <History />
+      <Form />
       <Footer />
     </>
   );
