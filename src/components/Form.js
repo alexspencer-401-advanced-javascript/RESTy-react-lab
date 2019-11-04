2e1d3279537330315d97476baec41b7d58098365
sanvean74 <dlambertpdx@gmail.com>
2019-11-04 09:57:02 -0800

    started css

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import apiCall from '../services/api-call';
 import Results from './Results';
 import History from './History';
+import styles from './Form.css';
 
 export default class Form extends Component {
 
@@ -30,22 +31,33 @@ export default class Form extends Component {
   render() {
     return (
       
-      <div>
-        <History history={this.state.history} />
+      <div className={styles.Form}>
+        <div className={styles.history}>
+          <History history={this.state.history} />
+        </div>
         <form onSubmit={this.handleSubmit}>
-          <input type="text" name="url" value={this.state.url} onChange={this.handleChange} />
+          <input className={styles.urlInput} type="text" name="url" value={this.state.url} onChange={this.handleChange} />
           <div>
-            <input type="radio" name="method" value="GET" onChange={this.handleChange} />GET
-            <input type="radio" name="method" value="PUT" onChange={this.handleChange} />PUT
-            <input type="radio" name="method" value="POST" onChange={this.handleChange} />POST
-            <input type="radio" name="method" value="PATCH" onChange={this.handleChange} />PATCH
-            <input type="radio" name="method" value="DELETE" onChange={this.handleChange} />DELETE
+            <label>
+              <input className={styles.radio} type="radio" name="method" value="GET" onChange={this.handleChange} />GET
+            </label>
+            <label>
+              <input className={styles.radio} type="radio" name="method" value="PUT" onChange={this.handleChange} />PUT
+            </label>
+            <label>
+              <input className={styles.radio} type="radio" name="method" value="POST" onChange={this.handleChange} />POST
+            </label>
+            <label>
+              <input className={styles.radio} type="radio" name="method" value="PATCH" onChange={this.handleChange} />PATCH
+            </label>
+            <label>
+              <input className={styles.radio} type="radio" name="method" value="DELETE" onChange={this.handleChange} />DELETE
+            </label>
             <button>Go!</button>
           </div>
           <textarea name="jsonInput" value={this.state.jsonInput} onChange={this.handleChange} cols="30" rows="10"></textarea>
-          {/* <input type="text" value={this.state.textArea.value} onChange={this.handleChange} /> */}
+          <Results results={this.state.apiData} />
         </form>
-        <Results results={this.state.apiData} />
       </div>
     
     );
